9578a6fffcd068d5f42a9976abf22add965d2b08
郭春日 <uochunri@gmail.com>
2017-08-25 21:43:53 +0800

    Create CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825132647) do
+ActiveRecord::Schema.define(version: 20170825133707) do
 
   create_table "directions", force: :cascade do |t|
     t.text     "step"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170825132647) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
